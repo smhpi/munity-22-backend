e58b8c6fece7e6c2f0f77c850e737581df533b84
Syed Poursaeid <poursaeid@outlook.com>
2019-02-23 00:27:59 -0500

    db-config

--- app/model/appModel.js
+++ app/model/appModel.js
@@ -1,74 +1,69 @@
 'user strict';
-var sql = require('./db.js');
+var sql = require('../../db.js');
 
 //Task object constructor
-var Product = function(prod){
+var Product = function (prod) {
     this.title = prod.title;
     this.body = prod.body;
     this.created_at = new Date();
 };
-Product.createProduct = function createUser(newProduct, result) {    
-        sql.query("INSERT INTO products set ?", newProduct, function (err, res) {
-                
-                if(err) {
-                    console.log("error: ", err);
-                    result(err, null);
-                }
-                else{
-                    console.log(res.insertId);
-                    result(null, res.insertId);
-                }
-            });           
+Product.createProduct = function createUser(newProduct, result) {
+    sql.query("INSERT INTO products set ?", newProduct, function (err, res) {
+
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+        } else {
+            console.log(res.insertId);
+            result(null, res.insertId);
+        }
+    });
 };
 Product.getProductById = function createUser(productId, result) {
-        sql.query("Select title, body, id from products where id = ? ", productId, function (err, res) {             
-                if(err) {
-                    console.log("error: ", err);
-                    result(err, null);
-                }
-                else{
-                    result(null, res);
-              
-                }
-            });   
+    sql.query("Select title, body, id from products where id = ? ", productId, function (err, res) {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+        } else {
+            result(null, res);
+
+        }
+    });
 };
 Product.getAllProduct = function getAllProduct(result) {
-        sql.query("Select * from products", function (err, res) {
+    sql.query("Select * from products", function (err, res) {
 
-                if(err) {
-                    console.log("error: ", err);
-                    result(null, err);
-                }
-                else{
-                  console.log('products : ', res);  
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+        } else {
+            console.log('products : ', res);
 
-                 result(null, res);
-                }
-            });   
+            result(null, res);
+        }
+    });
 };
-Product.updateById = function(id, prod, result){
-  sql.query("UPDATE products SET title = ? , body = ? WHERE id = ?", [prod.title,prod.body, id], function (err, res) {
-          if(err) {
-              console.log("error: ", err);
-                result(null, err);
-             }
-           else{   
-             result(null, res);
-                }
-            }); 
+Product.updateById = function (id, prod, result) {
+    sql.query("UPDATE products SET title = ? , body = ? WHERE id = ?", [prod.title, prod.body, id], function (err, res) {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+        } else {
+            result(null, res);
+        }
+    });
 };
-Product.remove = function(id, result){
-     sql.query("DELETE FROM products WHERE id = ?", [id], function (err, res) {
+Product.remove = function (id, result) {
+    sql.query("DELETE FROM products WHERE id = ?", [id], function (err, res) {
 
-                if(err) {
-                    console.log("error: ", err);
-                    result(null, err);
-                }
-                else{
-               
-                 result(null, res);
-                }
-            }); 
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+        } else {
+
+            result(null, res);
+        }
+    });
 };
 
-module.exports= Product;
+module.exports = Product;
--- db.js
+++ db.js
@@ -1,16 +1,16 @@
 'user strict';
-
+require('custom-env').env();
 var mysql = require('mysql');
 
 //local mysql db connection
 var connection = mysql.createConnection({
-    host: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`,
+    host: process.env.SQL_HOST,
     user: process.env.SQL_USER,
     password: process.env.SQL_PASSWORD,
     database: process.env.SQL_DATABASE
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
 });
 
