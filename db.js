4f408a45e5cf69c79217449c28b8f1dd62ca27fa
Syed Poursaeid <poursaeid@outlook.com>
2019-03-02 23:48:45 -0500

    fix-db-for-cloud

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
-'user strict';
-require('custom-env').env();
-const mysql = require('mysql');
+"user strict";
+//require("custom-env").env();
+const mysql = require("mysql");
 
-//local mysql db connection
+/*
 var connection = mysql.createConnection({
     user: process.env.SQL_USER,
     password: process.env.SQL_PASSWORD,
@@ -10,11 +10,17 @@ var connection = mysql.createConnection({
     host : process.env.SQL_HOST
     
 });
+*/
+// google cloud connection
+var connection = mysql.createConnection({
+  user: "userapp",
+  password: "Dooste,2004",
+  database: "csdepotdb",
+  socketPath: "/cloudsql/protean-impact-232500:us-central1:mydb"
+});
 
-
-
-connection.connect(function (err) {
-    if (err) throw err;
+connection.connect(function(err) {
+  if (err) throw err;
 });
 
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,47 +1,27 @@
-const express = require('express'),
+const express = require("express"),
   app = express(),
-  bodyParser = require('body-parser');
-  port = process.env.PORT;
- // require('custom-env').env();
-const mysql = require('mysql');
+  bodyParser = require("body-parser");
+port = process.env.PORT;
+// require('custom-env').env();
+const mysql = require("mysql");
 
 // connection configurations
 const mc = mysql.createConnection({
-  user: 'userapp',
-  password: 'Dooste,2004',
-  database: 'csdepotdb',
-  socketPath : '/cloudsql/protean-impact-232500:us-central1:mydb'
+  user: "userapp",
+  password: "Dooste,2004",
+  database: "csdepotdb",
+  socketPath: "/cloudsql/protean-impact-232500:us-central1:mydb"
 });
- 
+
 // connect to database
 
 mc.connect();
 
 app.listen(port);
-console.log('API server started on Port: ' + port);
-app.use('/',express.static('front'))
+console.log("API server started on Port: " + port);
+app.use("/", express.static("front"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-/*
-app.post('/product/bestbuy', function(req, res) {
 
-  var jsondata = req.body;
-  console.log(jsondata);
-  var values = [];
-  
-  for(var i=0; i< jsondata.length; i++){
-    values.push([jsondata[i].product_title,jsondata[i].shop_sku,jsondata[i].quantity ]);
-  }
-  //Bulk insert using nested array [ [a,b],[c,d] ] will be flattened to (a,b),(c,d)
-  mc.query('INSERT INTO bestbuy (title, sku, quantity) VALUES ?', [values], function(err,result) {
-    if(err) {
-       res.send('Error');
-    }
-   else {
-       res.send('Success');
-    }
-  });
-  });
-  */
-var routes = require('./app/routes/approutes'); //importing route
+var routes = require("./app/routes/approutes"); //importing route
 routes(app); //register the route
